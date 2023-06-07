99d8490811bd6631132ac2ac45741f9f3bca6767
lin <1583427850@qq.com>
2023-06-07 21:31:01 +0800

    Initial commit

---
+++ src/main/java/com/example/draw/DrawApplication.java
@@ -0,0 +1,13 @@
+package com.example.draw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/draw/DrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.draw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
